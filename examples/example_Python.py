859c839e7255528eb5bf49663bda96e6e96a686c
gschnabel <40870991+gschnabel@users.noreply.github.com>
2019-07-22 12:10:49 +0200

    modified example files

--- examples/example_Python.py
+++ examples/example_Python.py
@@ -1,42 +1,61 @@
+# load modules
 import pymongo
-import numpy as np
 from pymongo import MongoClient
+
+import numpy as np
 import pandas as pd
 from pandas.io.json import json_normalize
+
 import re
 
+# connect to MongoDB 
 client = MongoClient()
 
 client = MongoClient('localhost', 27017,
         username = "user", password = "pw")
 
+# choose database exfor
 db = client["exfor"]
+# and collection entries
 entries = db["entries"]
 
+# retrieve dataset with a specific subentry ID
 subent = entries.find_one({ 'ID': '11701004' })
 
+# explore a bit
+subent["BIB"]["AUTHOR"]
+subent["DATA"]["UNIT"]
+subent["DATA"]["TABLE"]
 
-
+# retrieve datasets whose REACTION string
+# matches a regular expression
 regex = re.compile("^\(26-FE-56\(N,[^)]+\)[^,]*,,SIG\)")
 subents = entries.find({ 'BIB.REACTION' : regex })
 
+# loop over the subents
+for subent in subents:
+    print(subent["BIB"]["AUTHOR"])
 
-tst = entries.find({'BIB.REACTION' : regex, 'DATA.TABLE.DATA': { '$exists' : True}}, 
-        {'ID': 1, 'BIB.AUTHOR': 1, 'DATA.TABLE.DATA': 1, '_id': 0 })
-
-df = json_normalize(list(tst))
+# impose additional requirement that
+# fields ID, BIB.AUTHOR and DATA.TABLE.DATA must be present
+# but not field _id
+query_spec = {'BIB.REACTION' : regex, 'DATA.TABLE.DATA': { '$exists' : True}}
+filter_spec = {'ID': 1, 'BIB.AUTHOR': 1, 'DATA.TABLE.DATA': 1, '_id': 0 }
+subents = entries.find(query_spec, filter_spec) 
+        
+# convert a subselection of the information
+# in the matching subentries to a pandas DataFrame
+rawdf = json_normalize(list(subents))
 # do some search operatoins
 lst_col = 'DATA.TABLE.DATA'
-rep_len = df[lst_col].str.len()
+rep_len = rawdf[lst_col].str.len()
 rep_len[np.isnan(rep_len)] = 1
 rep_len = rep_len.astype(int)
 
-r = pd.DataFrame({
-          col: np.repeat(df[col].values, rep_len)
-               for col in df.columns.drop(lst_col) }
-                ).assign(**{lst_col:np.hstack(df[lst_col].values)})[df.columns]
+df = pd.DataFrame({
+          col: np.repeat(rawdf[col].values, rep_len)
+               for col in rawdf.columns.drop(lst_col) }
+                ).assign(**{lst_col:np.hstack(rawdf[lst_col].values)})[rawdf.columns]
 
+print(df)
 
-tst = entries.find_one({'ID' : '10238034'})
-
-df['DATA.TABLE.DATA']
--- examples/example_R.R
+++ examples/example_R.R
@@ -1,25 +1,32 @@
-library(ongolite)
+library(mongolite)
 
 db <- mongo(collection = "entries",
             db = "exfor",
-            url = "mongodb://user:password@localhost:27017")
+            url = "mongodb://user:pw@localhost:27017")
 
+# retrieve subentry with specific ID
 it <- db$iterate('{ "ID": "11701004" }')
 subent <- it$one()
 
+# explore the subentry a bit
+subent$BIB$AUTHOR
+subent$DATA$UNIT
+subent$DATA$TABLE
+
+# search for subentry whose REACTION string
+# matches a specific regular expression
 regex <- "^\\\\(26-FE-56\\\\(N,[^)]+\\\\)[^,]*,,SIG\\\\)" 
 query <- paste0('{ "BIB.REACTION" : { "$regex": "', regex, '", "$options" : "" }, "DATA.TABLE.DATA" : { "$exists": 1}}')
 fields <- '{ "ID": 1, "BIB.AUTHOR": 1, "DATA.TABLE.DATA": 1, "_id": 0 }' 
 it <- db$iterate(query, fields)
 
+# loop over the found subentries
 while (!is.null(subent <- it$one())) {
-   print(subent) 
+   print(subent$BIB$AUTHOR) 
 }
 
-
-regex <- "^\\\\(26-FE-56\\\\(N,[^)]+\\\\)[^,]*,,SIG\\\\)" 
-query <- paste0('{ "BIB.REACTION" : { "$regex": "', regex, '", "$options" : "" }}')
-fields <- '{ "ID": 1, "BIB.AUTHOR": 1, "DATA.TABLE.DATA": 1, "_id": 0 }' 
-it <- db$find(query, fields)
-
+# consider using the R package at
+# https://github.com/gschnabel/MongoEXFOR
+# for a shallow wrapper around mongolite
+# with convenience functions
 
